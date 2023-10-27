ccf513044378fdd5002626ff8d7d6b6032707491
brandon-marzo <brandon.marzo@ibm.com>
2023-10-27 11:04:00 -0300

    Created the Controller of products

--- src/main/java/com/example/Produtos/controller/Products.java
+++ src/main/java/com/example/Produtos/controller/Products.java
@@ -1,6 +1,5 @@
 package com.example.Produtos.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +7,6 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class Products {
@@ -22,9 +20,20 @@ public class Products {
     }
 
     @GetMapping("/produtos/{id}")
-    public ResponseEntity<?> detalhar(@PathVariable("id") Integer id) {
-        usuario =
+    public Object[] detalhar(@PathVariable("id") Integer id) {
+        String uri = "https://dummyjson.com/products/" + id;
+        RestTemplate template = new RestTemplate();
+        return new Object[]{template.getForObject(uri, Object.class)};
     }
+
+    @GetMapping("/produtos/search/{search}")
+    public List<Object> searchOfProduct(@PathVariable("search") String search) {
+        String uri = "https://dummyjson.com/products/search?q=phone" + search;
+        RestTemplate template = new RestTemplate();
+        Object[] objects = new Object[]{template.getForObject(uri, Object.class)};
+        return Arrays.asList(objects);
+    }
+
 
 
 }
